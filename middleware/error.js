b6a5e0143bec5a9991dd8226f81814f523ed1393
ShandilyaHarsh <shandilyaharsh2001@gmail.com>
2021-09-05 23:56:39 +0530

    async, error middleware added

--- middleware/error.js
+++ middleware/error.js
@@ -1,25 +1,38 @@
-const errorResponse=require('../utils/errorResponse');
+const ErrorResponse = require('../utils/errorResponse');
 
-const errorHandler=(err,req,res,next)=>{
-    let error={...err}
-    error.message=err.message;
-    //Log to console for dev
-    console.log(err.stack.red);
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message;
+  //Log to console for dev
+  console.log(err.stack.red);
 
-    //Mongoose bad objectId
-    if(err.name==='CastError')
-    {
-        const message = `Resource not found with id of ${err.value}`;
-        error=new errorResponse(message,404);
+  //Mongoose bad objectId
+  if (err.name === 'CastError') {
+    const message = `Resource not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
 
-    }
+  //Mongoose Duplicate key
+  
+      if(err.code===11000){
+          const message=`duplicate field value entered`;
+          error=new ErrorResponse(message,400);
+      }
+  
 
-    console.log(err.name);
-    res.status(error.statusCode||500).json({
-        success:false,
-        error:error.message||'Server Error'
-    });
+  //Mongoose Validation error
+  if(err.name==='ValidationError')
+  {
+      const message=Object.values(err.errors).map(val=>val.message);
+      error=new ErrorResponse(message,400);
+  }
+   
 
-}
+  console.log(err.name);
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || 'Server Error',
+  });
+};
 
-module.exports=errorHandler;
+module.exports = errorHandler;
